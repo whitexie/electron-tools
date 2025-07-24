e656c9633c5459b7fc89a67128a5cbbe4738ca10
ysansan <hippeauxie@gmail.com>
2025-07-24 11:54:08 +0800

    chore: 引入 eslint-plugin-better-tailwindcss用于格式化tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss'
+import eslintParserVue from 'vue-eslint-parser'
 import nuxt from './.nuxt/eslint.config.mjs'
 
 export default await nuxt(
@@ -8,15 +10,38 @@ export default await nuxt(
       vue: true,
       typescript: true,
       pnpm: true,
-
       rules: {
         // Allow console statements in development
         'no-console': 'warn',
-
         // TypeScript rules
         '@typescript-eslint/no-explicit-any': 'error',
         '@typescript-eslint/ban-ts-comment': 'warn',
       },
+
     },
   ),
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: eslintParserVue,
+    },
+  },
+  {
+    plugins: {
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+    },
+    rules: {
+      // enable all recommended rules to report a warning
+      ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
+      // enable all recommended rules to report an error
+      ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+      // 'better-tailwindcss/no-unregistered-classes': 'off',
+    },
+    settings: {
+      'better-tailwindcss': {
+        // tailwindcss 4: the path to the entry file of the css based tailwind config (eg: `src/global.css`)
+        entryPoint: 'app/assets/css/main.css',
+      },
+    },
+  },
 )
